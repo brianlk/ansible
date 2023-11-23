00c73a9c2b76d637b9fd03614647dcb5ce73aad4
brian <brian.leung@techdata.com>
2023-11-23 18:00:02 +0800

    updates

--- dns-slave-to-master/scp-from-master.sh
+++ dns-slave-to-master/scp-from-master.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+exec 1>>/tmp/scp-from-master.log 
+exec 2>&1
+
+
 if [ $# -ne 1 ]; then
     echo "ERROR: No master ip is provided."
     exit 1
@@ -8,6 +12,8 @@ fi
 DEST="/var/named/standby"
 MIP=$1
 
-test -d $DEST || ( mkdir -p $DEST && chown named:named $DEST )
+test -d $DEST || mkdir -p $DEST
+date
 scp -pr $MIP:/var/named/data/* $DEST
 scp -pr $MIP:/etc/named.conf /etc/named.conf
+chown -R named:named $DEST
