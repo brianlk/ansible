dc737c68ea2f10e97970fe32f22ab293d61d7c4c
brian <brian.leung@techdata.com>
2024-01-09 16:45:01 +0800

    updates

--- vmware-shutdown/check_vm.py
+++ vmware-shutdown/check_vm.py
@@ -42,8 +42,8 @@ def main():
     parser = cli.Parser()
     args = parser.get_args()
     si = service_instance.connect(args)
-    # content = si.RetrieveContent()
-    # esx_host = pchelper.get_obj(content, [vim.HostSystem], "10.1.23.100")
+    content = si.RetrieveContent()
+    esx_host = pchelper.get_obj(content, [vim.HostSystem], "10.1.23.100")
     # print(esx_host)
     # rp = pchelper.search_for_obj(content, [vim.ResourcePool], "resgroup-8")
     # folder = content.searchIndex.FindByInventoryPath("/")
@@ -51,27 +51,27 @@ def main():
     #                               name="new vm name", asTemplate=False, pool=rp, host=esx_host)
     # view = pchelper.get_container_view(si, obj_type=[vim.ResourcePool])
     # print(view.type)
-    # datacenter = si.content.rootFolder.childEntity[0]
-    # view_manager = si.content.viewManager
-    # container_view = view_manager.CreateContainerView(datacenter, [vim.ResourcePool], True)
-    # largest_rp = None
-    # unreserved_for_vm = 0
-    # try:
-    #     for resource_pool in container_view.view:
-    #         if resource_pool.runtime.memory.unreservedForVm > unreserved_for_vm:
-    #             largest_rp = resource_pool
-    #             unreserved_for_vm = resource_pool.runtime.memory.unreservedForVm
-    # finally:
-    #     container_view.Destroy()
+    datacenter = content.rootFolder.childEntity[0]
+    view_manager = content.viewManager
+    container_view = view_manager.CreateContainerView(datacenter, [vim.ResourcePool], True)
+    largest_rp = None
+    unreserved_for_vm = 0
+    try:
+        for resource_pool in container_view.view:
+            if resource_pool.runtime.memory.unreservedForVm > unreserved_for_vm:
+                largest_rp = resource_pool
+                unreserved_for_vm = resource_pool.runtime.memory.unreservedForVm
+    finally:
+        container_view.Destroy()
     # content = si.RetrieveContent()
     # esx_host = pchelper.get_obj(content, [vim.HostSystem], "10.1.23.100")
-    # folder = si.RetrieveContent().searchIndex.FindByInventoryPath("/")
+    folder = pchelper.get_obj(content, [vim.Folder], "[san-1] abc1")
     # print(type(folder))
-    # task = folder.RegisterVM_Task(path="[san-1] abc1/abc1.vmx", 
-    #                                name="abc1", asTemplate=False, pool=largest_rp, host=esx_host)
-    datacenter = si.content.rootFolder.childEntity[0]
-    f = datacenter.vmFolder
-    print(type(f))
+    task = folder.RegisterVM_Task(path="[san-1] abc1/abc1.vmx", 
+                                    name="abc1", asTemplate=False, pool=largest_rp, host=esx_host)
+    # datacenter = si.content.rootFolder.childEntity[0]
+    # f = datacenter.vmFolder
+    # print(type(f))
 
 if __name__ == '__main__':
     main()
