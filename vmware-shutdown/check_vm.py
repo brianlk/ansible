1ebb58b81c500d0edf6aa8c065253693e817f63c
brian <brian.leung@techdata.com>
2024-01-09 16:30:01 +0800

    updates

--- vmware-shutdown/check_vm.py
+++ vmware-shutdown/check_vm.py
@@ -51,24 +51,27 @@ def main():
     #                               name="new vm name", asTemplate=False, pool=rp, host=esx_host)
     # view = pchelper.get_container_view(si, obj_type=[vim.ResourcePool])
     # print(view.type)
+    # datacenter = si.content.rootFolder.childEntity[0]
+    # view_manager = si.content.viewManager
+    # container_view = view_manager.CreateContainerView(datacenter, [vim.ResourcePool], True)
+    # largest_rp = None
+    # unreserved_for_vm = 0
+    # try:
+    #     for resource_pool in container_view.view:
+    #         if resource_pool.runtime.memory.unreservedForVm > unreserved_for_vm:
+    #             largest_rp = resource_pool
+    #             unreserved_for_vm = resource_pool.runtime.memory.unreservedForVm
+    # finally:
+    #     container_view.Destroy()
+    # content = si.RetrieveContent()
+    # esx_host = pchelper.get_obj(content, [vim.HostSystem], "10.1.23.100")
+    # folder = si.RetrieveContent().searchIndex.FindByInventoryPath("/")
+    # print(type(folder))
+    # task = folder.RegisterVM_Task(path="[san-1] abc1/abc1.vmx", 
+    #                                name="abc1", asTemplate=False, pool=largest_rp, host=esx_host)
     datacenter = si.content.rootFolder.childEntity[0]
-    view_manager = si.content.viewManager
-    container_view = view_manager.CreateContainerView(datacenter, [vim.ResourcePool], True)
-    largest_rp = None
-    unreserved_for_vm = 0
-    try:
-        for resource_pool in container_view.view:
-            if resource_pool.runtime.memory.unreservedForVm > unreserved_for_vm:
-                largest_rp = resource_pool
-                unreserved_for_vm = resource_pool.runtime.memory.unreservedForVm
-    finally:
-        container_view.Destroy()
-    content = si.RetrieveContent()
-    esx_host = pchelper.get_obj(content, [vim.HostSystem], "10.1.23.100")
-    folder = si.RetrieveContent().searchIndex.FindByInventoryPath("/")
-    print(type(folder))
-    task = folder.RegisterVM_Task(path="[san-1] abc1/abc1.vmx", 
-                                   name="abc1", asTemplate=False, pool=largest_rp, host=esx_host)
+    f = datacenter.vmFolder
+    print(type(f))
 
 if __name__ == '__main__':
     main()
