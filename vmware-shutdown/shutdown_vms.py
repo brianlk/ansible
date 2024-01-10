662ef70399779dffecef9c27ff2df589f28ac40a
brian <brian.leung@techdata.com>
2024-01-10 15:00:02 +0800

    updates

--- vmware-shutdown/shutdown_vms.py
+++ vmware-shutdown/shutdown_vms.py
@@ -13,26 +13,32 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
 
-def check_dc(content, datacenter_name, uuid):
+def check_vm_in_dc(content, datacenter_name, uuid):
     DC = pchelper.get_obj(content, [vim.Datacenter], datacenter_name)
     SI = content.searchIndex.FindByUuid(DC, uuid, True)
-    print(type(SI))
+    if isinstance(SI, vim.VirtualMachine):
+        return True
+    return False
 
 
 def shut_down(vm_name, ans = 'n'):
+    print("xxxxxxxxxxx")
     parser = cli.Parser()
     parser.add_required_arguments(cli.Argument.DATACENTER_NAME)
     args = parser.get_args()
     si = service_instance.connect(args)
-
+    print("xxxxxxxxxxx")
     VM = None
     content = si.RetrieveContent()
-    VM = pchelper.get_obj(content, [vim.VirtualMachine], vm_name)
-
-    check_dc(content, args.datacenter_name, VM.config.uuid)
-
-    exit()
+    try:
+        VM = pchelper.get_obj(content, [vim.VirtualMachine], vm_name)
+    except:
+        print("eeeeeeeeee")
+    print("xxxxxxxxxxx")
+    print(VM)
     if VM is None:
+        raise SystemExit("Unable to locate VirtualMachine.")
+    if not check_vm_in_dc(content, args.datacenter_name, VM.config.uuid):
         raise SystemExit("Unable to locate VirtualMachine.")
 
     while VM.runtime.powerState != "poweredOff":
@@ -56,6 +62,7 @@ def main():
         file_content = file.read()
     vms = file_content.split('\n')
     count = 0
+
     with ThreadPoolExecutor(max_workers=10) as executor:
         results = [executor.submit(shut_down, vm.strip()) for vm in vms if not vm.startswith('#')]
         for result in as_completed(results):
