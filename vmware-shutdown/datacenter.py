8e6c6b8fc7091407088398c5b85dbb0afa735e3b
brian <brian.leung@techdata.com>
2024-01-12 10:45:01 +0800

    updates

--- vmware-shutdown/datacenter.py
+++ vmware-shutdown/datacenter.py
@@ -8,19 +8,37 @@
 from tools import cli, service_instance, tasks, pchelper
 from pyVmomi import vim
 
+from json import JSONEncoder
+
+
+# subclass JSONEncoder
+class EmployeeEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+
 
 def config_snapshot(si, datacenter_name):
     content = si.RetrieveContent()
     DATACENTER = pchelper.get_obj(content, [vim.Datacenter], datacenter_name)
-    obj_view = content.viewManager.CreateContainerView(DATACENTER.vmFolder, [vim.Folder], True)
+    obj_view = content.viewManager.CreateContainerView(DATACENTER.vmFolder, 
+                                                       [vim.Folder], True)
     folder_list = obj_view.view
-    xxx = None
+    results = []
     for folder in folder_list:
         if isinstance(folder.childEntity, list):
             for v in folder.childEntity:
+                obj = Object()
                 if isinstance(v, vim.VirtualMachine):
-                    print(v.name, v.config.uuid, folder, v.summary.runtime.host, v.summary.config.vmPathName)
+                    obj.name = v.name
+                    obj.uuid = v.config.uuid
+                    obj.folder = folder
+                    obj.host = v.summary.runtime.host
+                    obj.vm_path = v.summary.config.vmPathName
+                    obj.resource_pool = v.resourcePool
+                    results.append(obj)
 
+    
+print(results.toJSON)
     # esx_host = pchelper.get_obj(content, [vim.HostSystem], "10.1.23.100")
     # for obj in esx_host.vm:
     #     print(obj.config.uuid)
