d47d40a7fe0d586f529ed630bd43230a0ac17d87
brian <brian.leung@techdata.com>
2024-01-15 14:45:01 +0800

    updates

--- vmware-shutdown/shutdown_vms.py
+++ vmware-shutdown/shutdown_vms.py
@@ -15,11 +15,11 @@ import time
 MAX_WORKERS_NUM = 10
 
 
-def shut_down(vm_name):
-    args = run_cli(cli.Argument.DATACENTER_NAME)
-    si = service_instance.connect(args)
-    content = si.RetrieveContent()
-    DATACENTER = pchelper.get_obj(content, [vim.Datacenter], args.datacenter_name)
+def shut_down(content, DATACENTER, vm_name):
+    # args = run_cli(cli.Argument.DATACENTER_NAME)
+    # si = service_instance.connect(args)
+    # content = si.RetrieveContent()
+    # DATACENTER = pchelper.get_obj(content, [vim.Datacenter], args.datacenter_name)
     dc_all_vm = None
     try:
         dc_all_vm = pchelper.get_all_obj(content, [vim.VirtualMachine], DATACENTER.vmFolder)
@@ -45,11 +45,15 @@ def shut_down(vm_name):
 
 def main():
     # Read the VM names from hosts file
+    args = run_cli(cli.Argument.DATACENTER_NAME)
+    si = service_instance.connect(args)
+    content = si.RetrieveContent()
+    DATACENTER = pchelper.get_obj(content, [vim.Datacenter], args.datacenter_name)
     VM_LIST = read_vm_list()
     count = 0
     # Parallel shutdown the VMs
     with ThreadPoolExecutor(max_workers=MAX_WORKERS_NUM) as executor:
-        results = [executor.submit(shut_down, vm.strip()) for vm in VM_LIST if not vm.startswith('#')]
+        results = [executor.submit(shut_down, content, DATACENTER, vm.strip()) for vm in VM_LIST if not vm.startswith('#')]
         for result in as_completed(results):
             if result._result:
                 count += 1
