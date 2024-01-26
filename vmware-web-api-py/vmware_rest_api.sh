613988805cd57d719945fc8bacf42cf879341771
brian <brian.leung@techdata.com>
2024-01-26 15:52:35 +0800

    updates

--- vmware-web-api-py/vmware_rest_api.sh
+++ vmware-web-api-py/vmware_rest_api.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-basic_auth_base64=$(echo -n 'administrator@vsphere.local:P@ssw0rd' | base64)
-session_id=$(curl --silent -k -XPOST -H "Authorization: Basic ${basic_auth_base64}" https://10.1.5.44/rest/com/vmware/cis/session)
-echo "$session_id"
+basic_auth_base64=$(echo -n 'administrator@vsphere.local:abcd1234' | base64)
+session_id_json=$(curl --silent -k -XPOST -H "Authorization: Basic ${basic_auth_base64}" https://10.1.5.44/rest/com/vmware/cis/session)
+session_id=$(echo "$session_id_json" | jq '.value')
+x=$(echo "$session_id" | sed -e 's/\"//g')
+curl -k -XGET -H 'vmware-api-session-id: '"$x"  https://10.1.5.44/rest/vcenter/vm
