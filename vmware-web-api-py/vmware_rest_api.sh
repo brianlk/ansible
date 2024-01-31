00a99b6552bffdd0ceb0516bc25ba1cac2e81ccf
brian <brian.leung@techdata.com>
2024-01-31 16:00:01 +0800

    updates

--- vmware-web-api-py/vmware_rest_api.sh
+++ vmware-web-api-py/vmware_rest_api.sh
@@ -9,5 +9,10 @@ x=$(echo "$session_id" | sed -e 's/\"//g')
 
 curl --silent -k -XGET -H 'vmware-api-session-id: '"$x"  https://$vcenter_ip/rest/vcenter/vm/vm-97 # get specific vm details
 
+# IBM hmc
 # curl -k -c cookies.txt -i -X PUT -H "Content-Type: application/vnd.ibm.powervm.web+xml; type=LogonRequest" -H "Accept: application/vnd.ibm.powervm.web+xml; type=LogonResponse" -H "X-Audit-Memento: hmc_test" -d@x.xml https://10.1.4.33/rest/api/web/Logon 
 #  curl -k -X GET -H "Content-Type: application/vnd.ibm.powervm.uom+xml; Type=ManagedSystem" -H "X-API-Session: NxHhMafg9RipO_8ERqZxVghS-uay7HRJhbrAFAuQDxQNXytBSjD6LtaAZgxBeizVLLBbojcCt7zmPKJwU1P60KW-TPmGXMQIUiq047Q6sipFjCcdaUiOWlu74B0QLJe8r4UlYQJAArVWC398-2s7UAL6ugsQKEVHlEQayxhqgI1s2iTUtKo_o8y6g_eGUuYZLe1rPW4jI6McrhhH2jid3yWek0PCfA0T6Eq69TjZRS4=" https://10.1.4.33/rest/api/uom/ManagedSystem
+
+# IBM v7000
+#  curl -XPOST  https://10.1.7.90:7443/rest/auth -H "accept: application/json" -H "X-Auth-Username: superuser" -H "X-Auth-Password: P@ssw0rd" -d "" -k
+# curl -XPOST  https://10.1.7.90:7443/rest/lssystem -H "accept: application/json" -H "X-Auth-Token: 85a65bafa2e18e549abcf9ea48007554adc6c5e029ef3935634ee8e54fe51b70" -k
